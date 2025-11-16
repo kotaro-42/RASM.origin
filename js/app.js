421ccd7038407072eae833fb1fdebcfeda2b949c
kotaro-42 <82247017+kotaro-42@users.noreply.github.com>
2025-11-16 16:26:58 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 async function setup() {
-    const patchExportURL = "export/patch.export.json";
+    const patchExportURL = "export/rasm_origin.json";
 
     // Create AudioContext
     const WAContext = window.AudioContext || window.webkitAudioContext;
